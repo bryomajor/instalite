f328bacce4a41b2626c60b0e63d720d3ecb0c72e
Brian Otieno <bryomajor@gmail.com>
2019-11-17 13:29:59 +0300

    create a profile model

--- instagram/models.py
+++ instagram/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+import datetime as datetime
+from django.contrib.auth.models import User
+from tinymce.models import HTMLField
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
+class Profile(models.Model):
+    bio = HTMLField()
+    photo = ImageField(blank = True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    
+    def __str__(self):
+        return self.bio
