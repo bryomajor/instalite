76febf799185ceb698365ee3d88a96bc6a0769b3
Brian Otieno <bryomajor@gmail.com>
2019-11-17 13:02:13 +0300

    create a view fucntion that will enable users activate their accounts

--- instagram/views.py
+++ instagram/views.py
@@ -38,5 +38,20 @@ def signup(request):
     return render(request, 'registration/signup.html', {'form': form})
 
 
+def activate(request, uidb64, token):
+    try:
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        login(request, user)
+        return HttpResponse('Thank you for your email confirmation. Now you can login your account.''<a href="/accounts/login/"> Click here </a>')
+    else:
+        return HttpResponse('Activation link is invalid!''<br> If you have an account <a href="/accounts/login/"> Log in here </a>')
+
+
 def timeline(request):
     return render(request, 'timeline/index.html', )
