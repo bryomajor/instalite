0599ea628f4eaf8476b78dded185401f49414b03
Brian Otieno <bryomajor@gmail.com>
2019-11-17 21:24:16 +0300

    create the profile form class

--- instagram/forms.py
+++ instagram/forms.py
@@ -7,3 +7,8 @@ class SignupForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['user']
